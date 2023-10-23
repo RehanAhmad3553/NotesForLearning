308b0a10c9c364d9d66b2ce80609da1c74c0c3e1
RehanAhmad3553 <117620259+RehanAhmad3553@users.noreply.github.com>
2023-10-23 12:17:27 +0500

    work here

---
+++ app/src/main/java/com/quizapplications/firstdesignclass/MainActivity.java
@@ -0,0 +1,25 @@
+package com.quizapplications.firstdesignclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.quizapplications.firstdesignclass.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+
+    }
+}
