310357554985739de8c835c6618c91341047aea7
RehanAhmad3553 <117620259+RehanAhmad3553@users.noreply.github.com>
2023-11-28 18:04:46 +0500

    new

--- app/src/main/java/com/quizapplications/firstdesignclass/MainActivity.java
+++ app/src/main/java/com/quizapplications/firstdesignclass/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        Toast.makeText(this, "abcxyz", Toast.LENGTH_SHORT).show();
+
 
 
     }
